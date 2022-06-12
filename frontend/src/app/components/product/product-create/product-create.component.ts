f6a0c1009275f7720e441907ee1cae960db39a8f
Paulo Roberto <79487813+Paulodiastst@users.noreply.github.com>
2022-06-12 18:08:49 -0300

    Fomulário Obter Produto

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,6 +1,7 @@
-import { ProductService } from '../product.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ProductService } from '../product.service';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-create',
@@ -9,19 +10,26 @@ import { Router } from '@angular/router';
 })
 export class ProductCreateComponent implements OnInit {
 
+  product: Product = {
+    name: '',
+    price: 1
+  }
+
   constructor(private productService: ProductService,
     private router: Router) { }
 
-  ngOnInit(): void {
-    
+  ngOnInit(): void {    
   }
+
   createProduct(): void {
-    this.productService.showMessage('Produto criado!')
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto criado!')
+      this.router.navigate(['/products'])
+    })
   }
 
   cancel(): void {
     this.router.navigate(['/products'])
-}
-
+  }
 }
   
